7887bea7661752c472862c04375f611ed10e778b
KATTASAHAN <kattasahan2234@gmail.com>
2024-02-11 12:21:39 +0530

    week-10 started

--- week-10/10-1-postgres/src/index.ts
+++ week-10/10-1-postgres/src/index.ts
@@ -1,3 +1,4 @@
+// write a function to create a users table in your database.
 import { Client } from "pg";
 
 const client = new Client({
@@ -5,12 +6,22 @@ const client = new Client({
     "postgresql://321710301025:0ajQCHhD3znt@ep-green-fire-42136329.us-east-2.aws.neon.tech/neondb?sslmode=require",
 });
 
-client.connect();
+async function createUsersTable() {
+  try {
+    await client.connect();
+    const result = await client.query(`
+          CREATE TABLE users (
+              id SERIAL PRIMARY KEY,
+              username VARCHAR(50) UNIQUE NOT NULL,
+              email VARCHAR(255) UNIQUE NOT NULL,
+              password VARCHAR(255) NOT NULL,
+              created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+          );
+      `);
+    console.log(result);
+  } catch (e) {
+    console.log("HI", e);
+  }
+}
 
-`CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    username VARCHAR(50) UNIQUE NOT NULL,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    create_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
-)`;
+createUsersTable();
