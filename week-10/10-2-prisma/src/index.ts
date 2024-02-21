3da247e27e0d89780696c65084f7c313bb25d4b6
KATTASAHAN <kattasahan2234@gmail.com>
2024-02-21 17:03:16 +0530

    Week 10-2-prisma Done / Week-10 Done

--- week-10/10-2-prisma/src/index.ts
+++ week-10/10-2-prisma/src/index.ts
@@ -8,8 +8,14 @@ type User = {
   firstName?: string;
   lastName?: string;
 };
-function InsertUser({ username, password, firstName, lastName }: User) {
-  prisma.user.create({
+
+type UpdateParams = {
+  firstName: string;
+  lastName: string;
+};
+
+async function insertUser({ username, password, firstName, lastName }: User) {
+  const res = await prisma.user.create({
     data: {
       username,
       password,
@@ -17,4 +23,65 @@ function InsertUser({ username, password, firstName, lastName }: User) {
       lastName,
     },
   });
+  return res;
 }
+
+async function updateUser(
+  username: string,
+  { firstName, lastName }: UpdateParams
+) {
+  const res = await prisma.user.update({
+    where: { username },
+    data: { firstName, lastName },
+  });
+  return res;
+}
+
+async function getUser(username: string) {
+  const res = await prisma.user.findFirst({ where: { username } });
+  return res;
+}
+
+async function createTodo(userId: number, title: string, description: string) {
+  const res = await prisma.todo.create({
+    data: {
+      title,
+      description,
+      userId,
+    },
+  });
+  return res;
+}
+
+async function getTodos(userId: number) {
+  const res = await prisma.todo.findMany({
+    where: { userId },
+  });
+  return res;
+}
+
+async function getTodosAndUserDetails(userId: number) {
+  const todos = await prisma.todo.findMany({
+    where: { userId },
+    select: {
+      user: true,
+      title: true,
+      description: true,
+    },
+  });
+  return todos;
+}
+
+console.log(
+  insertUser({
+    username: "user@hmail.com",
+    password: "password",
+    firstName: "urfirst",
+    lastName: "urlast",
+  }),
+  updateUser("user@hmail.com", { firstName: "first", lastName: "last" }),
+  getUser("user@hmail.com"),
+  createTodo(1, "title", "description"),
+  getTodos(1),
+  getTodosAndUserDetails(1)
+);
