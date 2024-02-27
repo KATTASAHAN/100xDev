9cbd4bb43eea8fd9453164b640c8dbdb84be3afd
KATTASAHAN <kattasahan2234@gmail.com>
2024-02-27 18:17:22 +0530

    cloudflare init

---
+++ week-11/11-1-Serverless-Fns/cloudflare/src/index.ts
@@ -0,0 +1,7 @@
+export interface Env {}
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		return Response.json({ message: 'Got a go to cloud' });
+	},
+};
